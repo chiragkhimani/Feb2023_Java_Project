b80b418f095481372b71a680b7d53964a9baa0db
chiragkhimani <khimanichirag@gmail.com>
2023-02-10 19:28:02 +0530

    Updated file with print message

--- src/com/java/class01/FirstJavaClass.java
+++ src/com/java/class01/FirstJavaClass.java
@@ -5,6 +5,7 @@ public class FirstJavaClass {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("Hello Java");
+		System.out.println("Hello");
 	}
 	
 }
